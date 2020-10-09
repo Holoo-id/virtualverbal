50b09f6fc015e72a22f022a938b67ee908292a31
Andika15Ramdani <41929305+Andika15Ramdani@users.noreply.github.com>
2020-10-09 16:44:29 +0700

    add livenews _ 20201009 - 1644

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5069,4 +5069,36 @@
     </div>
     <!-- NAVIGATION -->
 </nav>
-<!-- /NAVIGATION WRAP -->
+
+<!-- LIVE NEWS WIDGET WRAP -->
+<div class="live-news-widget-wrap">
+    <div class="live-news-widget grid-limit">
+        <div class="live-news-widget-stairs left red">
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+        </div>
+        <div class="live-news-widget-stairs right red">
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+            <div class="live-news-widget-stair"></div>
+        </div>
+        <div class="live-news-widget-title-wrap">
+            <img class="live-news-widget-icon" src="{{ asset('/frontend/assets/img/icons/live-news-icon.png')}}" alt="live-news-icon">
+            <p class="live-news-widget-title">Live News</p>
+        </div>
+        <div id="lineslide-wrap1" class="live-news-widget-text-wrap">
+            <p class="live-news-widget-text"></p>
+        </div>
+    </div>
+</div>

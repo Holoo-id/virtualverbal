aee5e87a9235d94b43ac06aa5d568865396c845c
Andika15Ramdani <41929305+Andika15Ramdani@users.noreply.github.com>
2020-10-20 15:05:50 +0700

    asdfghjkl

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -49,6 +49,53 @@
                             <div class="section-title-separator"></div>
                         </div>
                         <!-- /POST OPEN TEXT -->
+                        
+                        <!-- POST AUTHOR INFO -->
+                        <div style="margin-top: 5px;margin-bottom:40px;display: flex; justify-content: center;" class="post-author-info-wrap">
+                            <!-- USER AVATAR -->
+                            <a href="search-results.html">
+                                <figure class="user-avatar tiny liquid">
+                                    <img src="{{ asset('/frontend/assets/img/users/01.jpg')}}" alt="user-01">
+                                </figure>
+                            </a>
+                            <!-- /USER AVATAR -->
+                            <p style="display: flex; justify-content: center; align-items: center" class="post-author-info small light">Oleh 
+                                <a href="search-results.html" class="post-author" style="margin-left:2px"> Dexter</a>
+                                <span class="separator">-</span>Senin, 19 Oktober 2020, 14:29 WIB
+                                <span class="separator">-</span>
+                                <!-- BUBBLE ORNAMENT -->
+                                <a href="#" class="bubble-ornament fb">
+                                    <!-- FACEBOOK ICON -->
+                                    <svg class="facebook-icon">
+                                        <use xlink:href="#svg-facebook"></use>
+                                    </svg>
+                                    <!-- /FACEBOOK ICON -->
+                                </a>
+                                <!-- /BUBBLE ORNAMENT -->
+    
+                                <!-- BUBBLE ORNAMENT -->
+                                <a href="#" class="bubble-ornament twt">
+                                    <!-- TWITTER ICON -->
+                                    <svg class="twitter-icon">
+                                        <use xlink:href="#svg-twitter"></use>
+                                    </svg>
+                                    <!-- /TWITTER ICON -->
+                                </a>
+                                <!-- /BUBBLE ORNAMENT -->
+    
+                                <!-- BUBBLE ORNAMENT -->
+                                <a href="#" class="bubble-ornament gplus">
+                                    <!-- GPLUS ICON -->
+                                    <svg class="gplus-icon">
+                                        <use xlink:href="#svg-gplus"></use>
+                                    </svg>
+                                    <!-- /GPLUS ICON -->
+                                </a>
+                                <!-- /BUBBLE ORNAMENT -->
+                            </p>
+                        </div>
+                        <!-- /POST AUTHOR INFO -->
+
                         <p class="post-open-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
                             incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco
                             laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate

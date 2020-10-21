9a7007ff0d8de62bf8614203bc3ddbb37a651667
Andika15Ramdani <41929305+Andika15Ramdani@users.noreply.github.com>
2020-10-21 13:55:03 +0700

    20201021 - 1354

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -17,17 +17,70 @@
                     <div class="section-title-separator"></div>
                 </div>
                 <!-- /SECTION TITLE WRAP -->
+                <img style="margin-top:22px" src="{{ asset('/frontend/assets/img/brand/logovvfullcolor.png')}}" alt="">
 
                 <!-- CONTACT INFO PREVIEW -->
                 <div class="contact-info-preview negative">
                     <p class="contact-info-preview-text">Lorem ipsum dolor sit amet, consectetur dasede do eiusmod <span class="highlighted">tempor</span> unt ut labore et dolore mag lere adveniam, quis rud citation laboris.</p>
-
                     <!-- CONTACT INFO PREVIEW EMAIL WRAP -->
                     <div class="contact-info-preview-email-wrap">
                         <i class="email-icon icon-envelope"></i>
                         <a href="mailto:info@pixeldiamond.com" class="contact-info-preview-email">info@pixeldiamond.com</a>
                     </div>
                     <!-- CONTACT INFO PREVIEW EMAIL WRAP -->
+                    <!-- SOCIAL LINKS -->
+                    <div class="social-links">
+                        <!-- BUBBLE ORNAMENT -->
+                        <a href="#" class="bubble-ornament fb">
+                            <!-- FACEBOOK ICON -->
+                            <svg class="facebook-icon">
+                                <use xlink:href="#svg-facebook"></use>
+                            </svg>
+                            <!-- /FACEBOOK ICON -->
+                        </a>
+                        <!-- /BUBBLE ORNAMENT -->
+
+                        <!-- BUBBLE ORNAMENT -->
+                        <a href="#" class="bubble-ornament twt">
+                            <!-- TWITTER ICON -->
+                            <svg class="twitter-icon">
+                                <use xlink:href="#svg-twitter"></use>
+                            </svg>
+                            <!-- /TWITTER ICON -->
+                        </a>
+                        <!-- /BUBBLE ORNAMENT -->
+
+                        <!-- BUBBLE ORNAMENT -->
+                        <a href="#" class="bubble-ornament insta">
+                            <!-- INSTAGRAM ICON -->
+                            <svg class="instagram-icon">
+                                <use xlink:href="#svg-instagram"></use>
+                            </svg>
+                            <!-- /INSTAGRAM ICON -->
+                        </a>
+                        <!-- /BUBBLE ORNAMENT -->
+
+                        <!-- BUBBLE ORNAMENT -->
+                        <a href="#" class="bubble-ornament twitch">
+                            <!-- TWITCH ICON -->
+                            <svg class="twitch-icon">
+                                <use xlink:href="#svg-twitch"></use>
+                            </svg>
+                            <!-- /TWITCH ICON -->
+                        </a>
+                        <!-- /BUBBLE ORNAMENT -->
+
+                        <!-- BUBBLE ORNAMENT -->
+                        <a href="#" class="bubble-ornament youtube">
+                            <!-- YOUTUBE ICON -->
+                            <svg class="youtube-icon">
+                                <use xlink:href="#svg-youtube"></use>
+                            </svg>
+                            <!-- /YOUTUBE ICON -->
+                        </a>
+                        <!-- /BUBBLE ORNAMENT -->
+                    </div>
+                    <!-- /SOCIAL LINKS -->
                 </div>  
                 <!-- /CONTACT INFO PREVIEW -->
             </div>
@@ -42,7 +95,6 @@
 <!-- FOOTER BOTTOM WRAP -->
 <div class="footer-bottom-wrap">
     <!-- FOOTER BOTTOM -->
-    <img src="{{ asset('/frontend/assets/img/brand/logovvfullcolor.png')}}" alt="">
     <div class="footer-bottom grid-limit">
         <p class="footer-bottom-text">© Copyright 2020<span class="separator">-</span><span class="brand"><a class="highlighted" href="">virtualverbal.com</a></span>. All Rights Reserved<span class="separator">|</span><span class="brand"><a class="highlighted" href="">virtualverbal.com</a></span> is part of <span class="brand"><a class="highlighted" href="https://holoo.id/">Holoo.id</a></span>.</p>
     </div>

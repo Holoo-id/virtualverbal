6a9295775dd11d9dba1678b769c58b702f3015e0
Hilmi Hilmi <hilmi.kyosou.080202@gmail.com>
2020-10-22 16:40:09 +0700

    Again, Migration

--- database/migrations/2020_10_09_000002_create_users_table.php
+++ database/migrations/2020_10_09_000002_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->integer('role_id')->default('1');
+            $table->foreignId('role_id')->nullable();
             $table->string('image')->nullable();
             $table->boolean('activated')->default(false);
             $table->timestamps();

c5b8e1cdeb8608d160d2c9b313be5a7ed116e281
Andika15Ramdani <41929305+Andika15Ramdani@users.noreply.github.com>
2020-10-28 18:23:51 +0700

    20201028 - 1823

--- resources/views/back/create-content.blade.php
+++ resources/views/back/create-content.blade.php
@@ -1,12 +1,19 @@
 @extends('back.layouts.base')
 @section('content')
     <div class="grid grid-cols-12 gap-6 mt-5">
-        <div class="intro-y col-span-12 lg:col-span-6">
+        <div class="intro-y box col-span-12 lg:col-span-4 p-10">
+            <div class="intro-y text-justify leading-relaxed">
+                <p class="mb-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                <p class="mb-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+            </div>
+        </div>
+        <div class="intro-y col-span-12 lg:col-span-8">
             <!-- BEGIN: Form Validation -->
             <div class="intro-y box">
                 <div class="flex flex-col sm:flex-row items-center p-5 border-b border-gray-200">
                     <h2 class="font-medium text-base mr-auto">
-                        Implementation
+                        Buat Konten
                     </h2>
                 </div>
                 <div class="p-5" id="basic-datepicker">
@@ -68,11 +75,15 @@
                                 </div>
                             </div>
                             <div class="mt-3">
-                                <label class="flex flex-col sm:flex-row"> Konten </label>
-                                 <textarea data-feature="all" id="summernote" class="summernote mt-2" name="editor"></textarea>
+                                <label class="flex flex-col sm:flex-row mb-2"> Konten </label>
+                                 <textarea id="summernote" class="summernote" name="editor"></textarea>
+                            </div>
+                            <div class="mt-3">
+                                <label class="flex flex-col sm:flex-row"> SEO <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">Harus diisi</span> </label>
+                                <input type="text" name="seo" class="input w-full border mt-2" placeholder="SEO" required>
                             </div>
 
-                            <div class="mt-3">
+                            <!-- <div class="mt-3">
                                 <label class="flex flex-col sm:flex-row"> Email <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">Required, email address format</span> </label>
                                 <input type="email" name="email" class="input w-full border mt-2" placeholder="example@gmail.com" required>
                             </div>
@@ -87,8 +98,8 @@
                             <div class="mt-3">
                                 <label class="flex flex-col sm:flex-row"> Comment <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">Required, at least 10 characters</span> </label>
                                 <textarea class="input w-full border mt-2" name="comment" placeholder="Type your comments" minlength="10" required></textarea>
-                            </div>
-                            <button type="submit" class="button bg-theme-1 text-white mt-5">Register</button>
+                            </div> -->
+                            <button type="submit" class="button bg-theme-1 text-white mt-5">Submit</button>
                         </form>
                     </div>
                 </div>
--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -1,1 +1,195 @@
 @extends('back.layouts.base')
+@section('content')
+    <div class="intro-y box px-5 pt-5 mt-5">
+        <div class="flex flex-col lg:flex-row border-b border-gray-200 pb-5 -mx-5">
+            <div class="flex flex-1 px-5 items-center justify-center lg:justify-start">
+                <div class="w-20 h-20 sm:w-24 sm:h-24 flex-none lg:w-32 lg:h-32 image-fit relative">
+                    <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="{{ asset('/backend/dist/images/profile-14.jpg') }}">
+                </div>
+                <div class="ml-5">
+                    <div class="w-24 sm:w-40 truncate sm:whitespace-normal font-medium text-lg">Robert De Niro</div>
+                    <div class="text-gray-600">Software Engineer</div>
+                </div>
+            </div>
+            <div class="flex mt-6 lg:mt-0 items-center lg:items-start flex-1 flex-col justify-center text-gray-600 px-5 border-l border-r border-gray-200 border-t lg:border-t-0 pt-5 lg:pt-0">
+                <div class="truncate sm:whitespace-normal flex items-center"> <i data-feather="mail" class="w-4 h-4 mr-2"></i> robertdeniro@left4code.com </div>
+                <div class="truncate sm:whitespace-normal flex items-center mt-3"> <i data-feather="instagram" class="w-4 h-4 mr-2"></i> Instagram Robert De Niro </div>
+                <div class="truncate sm:whitespace-normal flex items-center mt-3"> <i data-feather="twitter" class="w-4 h-4 mr-2"></i> Twitter Robert De Niro </div>
+            </div>
+            <div class="mt-6 lg:mt-0 flex-1 flex items-center justify-center px-5 border-t lg:border-0 border-gray-200 pt-5 lg:pt-0">
+                <div class="text-center rounded-md w-full py-3">
+                    <div class="text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quo sint nobis architecto, molestiae praesentium quaerat beatae voluptatibus expedita, eveniet sequi excepturi voluptates? Blanditiis, eos unde ut qui ad tempora!</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-12 gap-6">
+        <div class="col-span-12 xxl:col-span-9 grid grid-cols-12 gap-6">
+            <!-- BEGIN: General Report -->
+            <div class="col-span-12 mt-8">
+                <div class="intro-y flex items-center h-10">
+                    <h2 class="text-lg font-medium truncate mr-5">
+                        General Report
+                    </h2>
+                    <a href="" class="ml-auto flex text-theme-1"> <i data-feather="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
+                </div>
+                <div class="grid grid-cols-12 gap-6 mt-5">
+                    <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                        <div class="report-box zoom-in">
+                            <div class="box p-5">
+                                <div class="flex">
+                                    <i data-feather="shopping-cart" class="report-box__icon text-theme-10"></i> 
+                                    <div class="ml-auto">
+                                        <div class="report-box__indicator bg-theme-9 tooltip cursor-pointer" title="33% Higher than last month"> 33% <i data-feather="chevron-up" class="w-4 h-4"></i> </div>
+                                    </div>
+                                </div>
+                                <div class="text-3xl font-bold leading-8 mt-6">4.510</div>
+                                <div class="text-base text-gray-600 mt-1">Item Sales</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                        <div class="report-box zoom-in">
+                            <div class="box p-5">
+                                <div class="flex">
+                                    <i data-feather="credit-card" class="report-box__icon text-theme-11"></i> 
+                                    <div class="ml-auto">
+                                        <div class="report-box__indicator bg-theme-6 tooltip cursor-pointer" title="2% Lower than last month"> 2% <i data-feather="chevron-down" class="w-4 h-4"></i> </div>
+                                    </div>
+                                </div>
+                                <div class="text-3xl font-bold leading-8 mt-6">3.521</div>
+                                <div class="text-base text-gray-600 mt-1">New Orders</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                        <div class="report-box zoom-in">
+                            <div class="box p-5">
+                                <div class="flex">
+                                    <i data-feather="monitor" class="report-box__icon text-theme-12"></i> 
+                                    <div class="ml-auto">
+                                        <div class="report-box__indicator bg-theme-9 tooltip cursor-pointer" title="12% Higher than last month"> 12% <i data-feather="chevron-up" class="w-4 h-4"></i> </div>
+                                    </div>
+                                </div>
+                                <div class="text-3xl font-bold leading-8 mt-6">2.145</div>
+                                <div class="text-base text-gray-600 mt-1">Total Products</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
+                        <div class="report-box zoom-in">
+                            <div class="box p-5">
+                                <div class="flex">
+                                    <i data-feather="user" class="report-box__icon text-theme-9"></i> 
+                                    <div class="ml-auto">
+                                        <div class="report-box__indicator bg-theme-9 tooltip cursor-pointer" title="22% Higher than last month"> 22% <i data-feather="chevron-up" class="w-4 h-4"></i> </div>
+                                    </div>
+                                </div>
+                                <div class="text-3xl font-bold leading-8 mt-6">152.000</div>
+                                <div class="text-base text-gray-600 mt-1">Unique Visitor</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END: General Report -->
+            <!-- BEGIN: Comments -->
+            <div class="col-span-12 xl:col-span-8 mt-6">
+                <div class="intro-y flex items-center h-10">
+                    <h2 class="text-lg font-medium truncate mr-5">
+                        Latest Comments
+                    </h2>
+                    <a href="" class="ml-auto flex text-theme-1"> <i data-feather="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
+                </div>
+                <div class="intro-y box mt-5 pb-10 px-8">
+                    <div class="pt-5">
+                        <div class="flex">
+                            <div class="w-10 h-10 sm:w-12 sm:h-12 flex-none image-fit">
+                                <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-3.jpg">
+                            </div>
+                            <div class="ml-3 flex-1">
+                                <div class="flex items-center"> <a href="" class="font-medium">Leonardo DiCaprio</a> <a href="" class="ml-auto text-xs text-gray-600">Reply</a> </div>
+                                <div class="text-gray-600 text-xs sm:text-sm">47 minutes ago</div>
+                                <div class="mt-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since the 1500</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-5 pt-5 border-t border-gray-200">
+                        <div class="flex">
+                            <div class="w-10 h-10 sm:w-12 sm:h-12 flex-none image-fit">
+                                <img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-10.jpg">
+                            </div>
+                            <div class="ml-3 flex-1">
+                                <div class="flex items-center"> <a href="" class="font-medium">Bruce Willis</a> <a href="" class="ml-auto text-xs text-gray-600">Reply</a> </div>
+                                <div class="text-gray-600 text-xs sm:text-sm">31 seconds ago</div>
+                                <div class="mt-2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END: Comments -->
+            <!-- BEGIN: Weekly Best Sellers -->
+            <div class="col-span-12 xl:col-span-4 mt-6">
+                <div class="intro-y flex items-center h-10">
+                    <h2 class="text-lg font-medium truncate mr-5">
+                        Weekly Best Sellers
+                    </h2>
+                </div>
+                <div class="mt-5">
+                    <div class="intro-y">
+                        <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                            <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                <img alt="Midone Tailwind HTML Admin Template" src="dist/images/profile-6.jpg">
+                            </div>
+                            <div class="ml-4 mr-auto">
+                                <div class="font-medium">Arnold Schwarzenegger</div>
+                                <div class="text-gray-600 text-xs">12 March 2022</div>
+                            </div>
+                            <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                        </div>
+                    </div>
+                    <div class="intro-y">
+                        <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                            <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                <img alt="Midone Tailwind HTML Admin Template" src="dist/images/profile-4.jpg">
+                            </div>
+                            <div class="ml-4 mr-auto">
+                                <div class="font-medium">Arnold Schwarzenegger</div>
+                                <div class="text-gray-600 text-xs">25 August 2022</div>
+                            </div>
+                            <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                        </div>
+                    </div>
+                    <div class="intro-y">
+                        <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                            <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                <img alt="Midone Tailwind HTML Admin Template" src="dist/images/profile-13.jpg">
+                            </div>
+                            <div class="ml-4 mr-auto">
+                                <div class="font-medium">Nicolas Cage</div>
+                                <div class="text-gray-600 text-xs">12 January 2022</div>
+                            </div>
+                            <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                        </div>
+                    </div>
+                    <div class="intro-y">
+                        <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                            <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                <img alt="Midone Tailwind HTML Admin Template" src="dist/images/profile-4.jpg">
+                            </div>
+                            <div class="ml-4 mr-auto">
+                                <div class="font-medium">Angelina Jolie</div>
+                                <div class="text-gray-600 text-xs">21 September 2022</div>
+                            </div>
+                            <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                        </div>
+                    </div>
+                    <a href="" class="intro-y w-full block text-center rounded-md py-4 border border-dotted border-theme-15 text-theme-16">View More</a> 
+                </div>
+            </div>
+            <!-- END: Weekly Best Sellers -->
+        </div>
+    </div>
+@endsection

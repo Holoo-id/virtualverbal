b0a3875e31a771f3362c7b66c2c37c8acf5b39c5
Andika .R <andika15ramdani@gmail.com>
2020-12-04 14:13:57 +0700

    20201204 - 1413 - add service igdb

---
+++ config/igdb.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+     * This is the API Token you got from https://api.igdb.com
+     */
+    'api_token' => env('IGDB_TOKEN', 'fae55dce240a1e74ea9612e68f3645be'),
+    
+    /*
+     * These are the credentials you got from https://dev.twitch.tv/console/apps
+     */
+    'credentials' => [
+        'client_id' => env('TWITCH_CLIENT_ID', ''),
+        'client_secret' => env('TWITCH_CLIENT_SECRET', ''),
+    ],
+
+    /*
+     * This package caches queries automatically (for 1 hour per default).
+     * Here you can set how long each query should be cached (in seconds).
+     *
+     * To turn cache off set this value to 0
+     */
+    'cache_lifetime' => env('IGDB_CACHE_LIFETIME', 3600),
+
+    /*
+     * This is the per-page limit for your tier.
+     */
+    'per_page_limit' => 500,
+];

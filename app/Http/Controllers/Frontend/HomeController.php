29791b7d9efeef711c5f382b2e46e408cbcb67bc
Andika .R <andika15ramdani@gmail.com>
2020-12-14 16:53:02 +0700

    20201214 - 1652 - still f****** image

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -7,6 +7,7 @@ use App\Models\Content;
 use App\Models\FormatContent;
 use App\Models\User;
 use Illuminate\Http\Request;
+use MarcReichel\IGDBLaravel\Models\Game;
 
 class HomeController extends Controller
 {
@@ -16,6 +17,11 @@ class HomeController extends Controller
         foreach ($contents as $content) {
             $content->publish_at = \Carbon\Carbon::parse($content->publish_at)->format('l, d F Y H:m');
         }
-        return view('front.home', compact('contents'));
+        $coming_soon = Game::with(['cover', 'release_dates'])
+            ->where('status', 4)
+            ->get()->sortByDesc('first_release_date');
+        $hypes = Game::with(['cover'])->where('status', 4)->get()->sortByDesc('hypes');
+        $recently_release = Game::with(['cover'])->get()->sortByDesc('first_release_date');
+        return view('front.home', compact('coming_soon', 'contents', 'hypes', 'recently_release'));
     }
 }
